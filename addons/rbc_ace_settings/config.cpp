cf5c1db22ab57896d738167a7243bf40d7895802
Zealot <zealot111@rambler.ru>
2016-11-13 23:30:46 +0300

    final

--- addons/rbc_ace_settings/config.cpp
+++ addons/rbc_ace_settings/config.cpp
@@ -1,47 +1,60 @@
-class CfgPatches {
-    class rbc_ace_settings {
-
-    };
-
-
-
-
+class CfgPatches
+{
+	class rbc_ace_settings
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"ace_interaction", "ace_repair",  "ace_common", "ace_frag", "ace_hearing", "ace_interact_menu", "ace_interaction", "ace_medical", "ace_hitreactions"};
+		author[] = {"Zealot"};
+		authorUrl = "";
+	};
 };
-
-
-class ACE_Settings {
-    //Minimum mamage needed to trigger falling down while moving. Set to -1 to disable completely.
-    class ace_hitreactions_minDamageToTrigger {
-        value = -1;
-        force = 1;
-        isClientSettable = 0;
-    };
-    class ace_medical_playerDamageThreshold {
-        value = 0.5;
-        force = 1;
-        isClientSettable = 0;
-    };
-    class ace_medical_preventInstaDeath {
-        value = 0;
-        force = 1;
-        isClientSettable = 0;
-    };
-    class ace_ace_frag_Enabled {
-        value = 1;
-        force = 1;
-        isClientSettable = 0;
-    };
-    class ace_hearing_disableEarRinging {
-        value = 1;
-        isClieentSettable = 0;
-
-    };
+class ACE_Settings
+{
+	class ace_hitreactions_minDamageToTrigger
+	{
+		value = -1;
+		force = 1;
+		isClientSettable = 0;
+	};
+	class ace_medical_playerDamageThreshold
+	{
+		value = 0.5;
+		force = 1;
+		isClientSettable = 0;
+	};
+	class ace_medical_preventInstaDeath
+	{
+		value = 0;
+		force = 1;
+		isClientSettable = 0;
+	};
+	class ace_ace_frag_Enabled
+	{
+		value = 1;
+		force = 1;
+		isClientSettable = 0;
+	};
+	class ace_hearing_disableEarRinging
+	{
+		value = 1;
+		isClieentSettable = 0;
+	};
+	class ace_hearing_enableCombatDeafness
+	{
+		value = 0;
+		isClientSettable = 0;
+	};
 };
-
-class ACE_Medical_Actions {
-    class Basic {
-        class Bandage {
-            treatmentTime = 10;
-        };
-    };
+class ACE_Medical_Actions
+{
+	class Basic
+	{
+		class Bandage
+		{
+			treatmentTime = 10;
+		};
+	};
 };
+//};
